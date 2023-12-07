f0b4dd7f2d082613dc91af632709e98f9d7b40ea
Aleksander <galuolek@gmail.com>
2023-12-07 15:07:17 +0100

    commit

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -1,12 +1,18 @@
 package algorithm;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] groszamount = {50000,20000,10000,5000,1000,500,200,100,50,20,10,5,2,1};
-        System.out.println(Arrays.toString(exchangeMoney(1005.24, groszamount)));
+        Scanner scaner = new Scanner(System.in);
+        int[] groszamount = {50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1};
+        System.out.println("Prosze zaplacić ileś tam złotych nwm");
+        double money = scaner.nextDouble();
+        System.out.println("Oto reszta: ");
+        System.out.println(Arrays.toString(exchangeMoney(money, groszamount)));
     }
     public static int[] exchangeMoney(double exchange, int[] groszamount){
         int size = groszamount.length;
