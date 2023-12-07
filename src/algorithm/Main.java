c55160a47a9b6a5e169cafebc62e260adbbfdc53
Aleksander <galuolek@gmail.com>
2023-12-07 15:41:31 +0100

    commit

--- src/algorithm/Main.java
+++ src/algorithm/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
+        //tablica nominałów w groszach
         int[] groszamount = {50000,20000,10000,5000,2000,1000,500,200,100,50,20,10,5,2,1};
         System.out.println("Cena produktu");
         double productPrice = scaner.nextDouble();
